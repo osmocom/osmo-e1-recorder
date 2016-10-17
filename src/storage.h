afb6d3990816f1290fc95fceafe1eac53e52bdb2
Harald Welte <laforge@gnumonks.org>
2016-10-17 18:50:19 +0200

    storage.h: document fields of header

--- src/storage.h
+++ src/storage.h
@@ -10,11 +10,17 @@ enum osmo_e1cap_capture_mode {
 
 /* header for each frame we store */
 struct osmo_e1cap_pkthdr {
+	/* Timestamp at which frame was received */
 	struct timeval ts;
+	/* length of frame data after this header */
 	uint32_t len;
+	/* line/span number on which frame was received */
 	uint8_t line_nr;
+	/* timeslot number on which frame was received */
 	uint8_t ts_nr;
+	/* see osmo_e1cap_capture_mode */
 	uint8_t capture_mode;
+	/* any optional future flags */
 	uint8_t flags;
 } __attribute__((aligned));
 
